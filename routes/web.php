70dfb3183a84b1d02711769b5e9ad5f70c66559c
Aamir Raza <amirraza14377143@gmail.com>
2025-09-22 19:52:20 +0500

    Now I add the route home name

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// Route to show hello
 Route::get('/hello',function(){
     return 'Hello this is my blog project';
+});
+
+// This is Home Page
+Route::get('/home',function(){
+    return 'This is my Home Page';
 });
