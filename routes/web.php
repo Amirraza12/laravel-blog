ceb2a52738a62c1e37b48caf2e4157cc4d0e9dac
Aamir Raza <amirraza14377143@gmail.com>
2025-09-24 00:15:18 +0500

    Day 2 Routing Basic (static,dynamic,groups

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,61 @@ Route::get('/hello',function(){
 Route::get('/home',function(){
     return 'This is my Home Page';
 });
+
+
+// About Route
+Route::get('/about',function(){
+    return 'This is About Page';
+});
+
+// Dynamic Routing
+Route::get('/user/{id}',function($id){
+    return 'User Profile ID : '. $id;
+});
+
+Route::get('/student/{id}',function($roll_no){
+    return 'This is Student Roll No'. $roll_no;
+});
+
+Route::get('/employee/{id}',function($id){
+    return 'Employee ID'. $id;
+});
+
+
+// Optional Parameters
+
+Route::get('/post/{id?}',function($id = null){
+    return $id ? "Post ID".$id : 'No Post ID Provided';
+});
+
+Route::get('/student_id/{id?}',function($id = null){
+    return $id ? 'Student ID '. $id : 'No Student ID Provided';
+});
+
+
+Route::get('/employee_id/{emp_id?}',function($empId = null){
+    return $empId ? 'Employee ID'. $empId : 'Not Available';
+
+});
+
+
+// Named Routes
+
+Route::get('/contact',function(){
+    return "contact Page";
+})->name('contact');
+
+Route::get('/go-contact',function(){
+    return redirect()->route('contact');
+});
+
+// Route Group Basic 
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard',function(){
+        return 'Welcome To Dashboard';
+    });
+
+    Route::get('/user',function(){
+        return "User Route is here";
+    });
+});;
